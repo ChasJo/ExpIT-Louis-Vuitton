e373c112c78499da4ed2640b00cf523f92d115da
MarkDarga <mlmdarga@gmail.com>
2018-11-21 07:03:41 -0500

    made start up page

---
+++ ExperienceIT-Final-Project/ExperienceIT-Final-Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExperienceIT_Final_Project.Startup))]
+namespace ExperienceIT_Final_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
