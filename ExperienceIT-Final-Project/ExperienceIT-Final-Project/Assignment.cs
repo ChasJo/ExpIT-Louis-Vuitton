520daa58ab9c9ece1bd5fad5bb85505d50dbce1c
yuktakodwani <kodwani.yukta@gmail.com>
2018-11-22 15:43:38 -0500

    added user base class and student class

---
+++ ExperienceIT-Final-Project/ExperienceIT-Final-Project/Assignment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExperienceIT_Final_Project
+{
+    public class Assignment
+    {
+        public string Description { get; set; }
+
+    }
+}
