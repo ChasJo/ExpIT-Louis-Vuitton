8781f575eee0ee350026dc1fa756470e71f4bbfa
yuktakodwani <kodwani.yukta@gmail.com>
2018-12-04 00:44:54 -0500

    updated the viewStudents

---
+++ ExperienceIT-Final-Project/ExperienceIT-Final-Project/Controllers/MentorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExperienceIT_Final_Project.Controllers
+{
+    public class MentorController : Controller
+    {
+        private readonly object students;
+
+
+        // GET: Mentor
+        public ActionResult Index()
+        {
+            // return View(students Progress<>);
+            return View();
+        }
+
+        public ActionResult ViewStudents()
+        {
+            return View();
+        }
+
+
+
+    }
+}
