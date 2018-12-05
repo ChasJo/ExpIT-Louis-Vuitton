99204df5f9a82488d19604b1dbbbf6b827de3305
MarkDarga <mlmdarga@gmail.com>
2018-12-04 19:09:57 -0500

    updated Layout for the student pages

--- ExperienceIT-Final-Project/ExperienceIT-Final-Project/Models/Student.cs
+++ ExperienceIT-Final-Project/ExperienceIT-Final-Project/Models/Student.cs
@@ -8,7 +8,7 @@ namespace ExperienceIT_Final_Project.Models
 {
     public class Student : User
     {
-        public int? LastAssignment { get; set; }
+        public int LastAssignment { get; set; }
 
 
         public List<Assignment> assignmentList = new List<Assignment>();
