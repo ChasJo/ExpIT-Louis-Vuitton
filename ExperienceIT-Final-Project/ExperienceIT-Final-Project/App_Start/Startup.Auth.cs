42aacc818a0f2b775682be8f08fee5859ba54555
MarkDarga <mlmdarga@gmail.com>
2018-12-11 08:31:50 -0500

    added google api

--- ExperienceIT-Final-Project/ExperienceIT-Final-Project/App_Start/Startup.Auth.cs
+++ ExperienceIT-Final-Project/ExperienceIT-Final-Project/App_Start/Startup.Auth.cs
@@ -58,11 +58,10 @@ namespace ExperienceIT_Final_Project
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(
+                clientId: "271090725875-djoadct38nujmvupqm9c1fchj89m39va.apps.googleusercontent.com",
+                clientSecret: "aokuNzyvIx7njNDIwX92QUcE"
+                );
         }
     }
 }
